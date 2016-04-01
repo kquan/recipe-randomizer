aa6d8fca6788f0d6ad669d2c241eee270a53aa6f
Kevin Quan <kevin.quan@samsung.com>
2016-04-01 11:07:40 -0400

    Initial commit

---
+++ android/app/src/main/java/com/kevinquan/recipe/randomizer/RecipeRandomizer.java
@@ -0,0 +1,17 @@
+package com.kevinquan.recipe.randomizer;
+
+import android.app.Application;
+
+/**
+ * Created by Kevin on 4/1/2016.
+ */
+public class RecipeRandomizer extends Application {
+
+    @SuppressWarnings("Unused")
+    private static final String TAG = RecipeRandomizer.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
